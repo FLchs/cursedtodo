5d4fa0bf35d7e70def1ebfa9261829d078178074
François Lachèse <francois.lachese@gmail.com>
2024-10-09 23:54:50 +0200

    chore: use ugly TYPE_CHECKING variable to prevent circular import

--- src/cursedtodo/views/main_view.py
+++ src/cursedtodo/views/main_view.py
@@ -1,25 +1,31 @@
-from typing import Any
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from cursedtodo.views.base_view import BaseView
+
+if TYPE_CHECKING:
+    from cursedtodo.controlers.main_controller import MainController
+
 
 
 class MainView(BaseView):
-    def __init__(self, controler: Any) -> None:
-        super().__init__(controler)
+    def __init__(self, controller: MainController) -> None:
+        super().__init__(controller)
+        self.controller = controller
         self.count = 0
 
     def render(self) -> None:
         self.window.erase()
         self.window.addstr(0, 0, "Main window")
         self.window.addstr(1, 0, str(self.count))
-        self.window.addstr(2,0, self.controller.data[0].summary)
+        self.window.addstr(2,0, self.controller.data[self.count].summary)
 
     def main_loop(self) -> None:
         while True:
             k = self.window.getch()
             if self.controller.handle_key(k):
                 break
-            elif k == ord("k"):
+            elif k == ord("k") and self.count < len(self.controller.data) -1:
                 self.count += 1
-            elif k == ord("j"):
+            elif k == ord("j") and self.count > 0:
                 self.count -= 1
             self.render()
