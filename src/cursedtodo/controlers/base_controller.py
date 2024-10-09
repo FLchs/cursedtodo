2717f4261e2f7069c99af51075cdd4c823497181
François Lachèse <francois.lachese@gmail.com>
2024-10-09 23:54:50 +0200

    chore: use ugly TYPE_CHECKING variable to prevent circular import

--- src/cursedtodo/controlers/base_controller.py
+++ src/cursedtodo/controlers/base_controller.py
@@ -1,10 +1,15 @@
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from abc import ABC, abstractmethod
 from curses import window
-from typing import Any
+
+if TYPE_CHECKING:
+    from cursedtodo.utils.router import Router
 
 
 class Controller(ABC):
-    def __init__(self, router: Any) -> None:
+    def __init__(self, router: Router) -> None:
         self.window: window = router.window
         self.router = router
 
