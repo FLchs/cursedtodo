2f57f4d88e9a3896108d4d122ddb597b9ffa5701
François Lachèse <francois.lachese@gmail.com>
2024-10-21 19:04:11 +0200

    feat: add basic todo creation view and controller

--- src/cursedtodo/utils/router.py
+++ src/cursedtodo/utils/router.py
@@ -1,10 +1,9 @@
 from curses import window
 
+from cursedtodo.controlers.create_todo_controller import CreateTodoController
 from cursedtodo.controlers.main_controller import MainController
 from cursedtodo.controlers.view_todo_controller import ViewTodoController
 from cursedtodo.models.todo import Todo
-
-
 
 
 class Router:
@@ -17,6 +16,9 @@ class Router:
 
     def route_view_todo(self, todo: Todo) -> None:
         ViewTodoController(self).run(todo)
+
+    def route_create_todo(self) -> None:
+        CreateTodoController(self).run()
 
     # def gotoB(self, data: str):
     #     bcontroller = BController(self.window, self, data = "nigga")
