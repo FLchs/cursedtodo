7de271cd83fd9e9bd9d08260b4530733859917e6
François Lachèse <francois.lachese@gmail.com>
2024-11-25 11:41:25 +0100

    feat: refactor form fields

---
+++ src/cursedtodo/views/form/select_field.py
@@ -0,0 +1,42 @@
+from collections.abc import Callable
+from curses import A_BOLD, KEY_RESIZE, window
+
+from cursedtodo.utils.selectinput import SelectInput
+from cursedtodo.views.form.base_field import BaseField
+
+
+class SelectField(BaseField):
+    def __init__(
+        self,
+        y: int,
+        window: window,
+        name: str,
+        id: str,
+        values: list[str],
+        validator: Callable[[int | str], int | str],
+    ):
+        super().__init__(y, window, name, id, validator, None)
+        self.value: str = ""
+        self.values = values
+        self.textwindow = window.derwin(1, 25, y, 15)
+        self.validator = validator
+        self.editor = SelectInput(self.textwindow, values, self._validator)
+
+    def _validator(self, ch: str | int) -> str | int:
+        if ch == KEY_RESIZE:
+            self.value = self.editor.gather().strip()
+        return ch
+
+    def render(self) -> None:
+        self.window.addstr(self.y, 1, f"{self.name}: ", A_BOLD)
+        self.textwindow.move(0, 0)
+        if self.value is not None and len(self.value) > 0:
+            self.editor.set_value(self.value)
+        else:
+            self.value = self.values[0]
+        self.editor.render()
+        # Editor().main(self.textwindow,self.value or "")
+
+    def focus(self) -> None:
+        self.editor.main()
+        self.value = self.editor.gather().strip()
