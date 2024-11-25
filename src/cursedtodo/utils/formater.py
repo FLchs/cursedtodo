68fa37e1b599df738cb58bafbec1316a672f3a2b
François Lachèse <francois.lachese@gmail.com>
2024-11-25 13:35:27 +0100

    fix: correct priority order according to VTODO specs

--- src/cursedtodo/utils/formater.py
+++ src/cursedtodo/utils/formater.py
@@ -1,65 +1,49 @@
 import curses
+from dataclasses import dataclass
+
+
+@dataclass
+class Priority:
+    index: int
+    value: str
+    color: int
 
 
 class Formater:
+    priorities = [
+        Priority(0, "No priority", curses.COLOR_WHITE),
+        Priority(10, "Lowest", curses.COLOR_WHITE),
+        Priority(9, "Very Low", curses.COLOR_BLUE),
+        Priority(8, "Low", curses.COLOR_CYAN),
+        Priority(7, "Below Average", curses.COLOR_GREEN),
+        Priority(6, "Average", curses.COLOR_YELLOW),
+        Priority(5, "Above Average", curses.COLOR_MAGENTA),
+        Priority(4, "High", curses.COLOR_RED),
+        Priority(3, "Very High", curses.COLOR_RED),
+        Priority(2, "Highest", curses.COLOR_RED),
+        Priority(1, "Critical", curses.COLOR_RED),
+    ]
+
+    @staticmethod
+    def init_priority_colors() -> None:
+        # Initialize color pairs for each priority
+        for priority in Formater.priorities:
+            curses.init_pair(10 + priority.index, priority.color, -1)
+
 
     @staticmethod
     def formatPriority(priority: int) -> tuple[str, int]:
-
         # Ensure the priority is within the valid range
         if priority < 0 or priority > 9:
             raise ValueError("Priority must be between 0 and 9")
 
-        # Define words and colors based on priority
-        words = [
-            "Lowest",
-            "Very Low",
-            "Low",
-            "Below Average",
-            "Average",
-            "Above Average",
-            "High",
-            "Very High",
-            "Highest",
-            "Critical",
-        ]
-        colors = [
-            curses.COLOR_WHITE,
-            curses.COLOR_BLUE,
-            curses.COLOR_CYAN,
-            curses.COLOR_GREEN,
-            curses.COLOR_YELLOW,
-            curses.COLOR_MAGENTA,
-            curses.COLOR_RED,
-            curses.COLOR_RED,
-            curses.COLOR_RED,
-            curses.COLOR_RED,
-        ]
-
-        # Initialize color pairs
-        for i, color in enumerate(colors):
-            curses.init_pair(i + 10, color, -1)
 
         # Get the word and color for the given priority
-        word = words[priority]
-        color_pair = curses.color_pair(priority + 10)
-        return word, color_pair
+        fmt_priority = next(p for p in Formater.priorities if p.index == priority)
+        return fmt_priority.value, curses.color_pair(fmt_priority.index + 10)
 
     @staticmethod
     def parse_priority(string: str | None) -> int:
-        if string is None :
+        if string is None:
             return 0
-        words = [
-            "Lowest",
-            "Very Low",
-            "Low",
-            "Below Average",
-            "Average",
-            "Above Average",
-            "High",
-            "Very High",
-            "Highest",
-            "Critical",
-        ]
-        return words.index(string) or 0
-
+        return next(p for p in Formater.priorities if p.value == string).index or 0
