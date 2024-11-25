1ac994c1c1b4ff5a0e7cc0d4962a1f1f27a6f65b
François Lachèse <francois.lachese@gmail.com>
2024-11-25 11:38:31 +0100

    feat: saving new and modified todos

--- src/cursedtodo/models/todo.py
+++ src/cursedtodo/models/todo.py
@@ -1,10 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
-import os
-from uuid import uuid1
+from datetime import datetime
 
-from arrow import Arrow
-from ics import Calendar, Todo as IcsTodo
+from ics import Todo as IcsTodo
 from ics.parsers.parser import ContentLine
 
 
@@ -17,58 +15,33 @@ class Todo:
     list: str
     path: str | None
     priority: int
-    completed: Arrow | None
-    due: Arrow | None
+    completed: datetime | None
+    due: datetime | None
     location: str | None
 
     def __lt__(self, other: Todo) -> bool:
         return self.priority < other.priority
 
-    def mark_as_done(self) -> None:
-        if self.path is None:
-            raise Exception("Todo path is none")
-        calendar = Calendar(open(self.path).read())
-        todo_item = next(
-            (todo for todo in calendar.todos if todo.uid == self.id),
-            None,
+    def _add_categories(self, todo_item: IcsTodo) -> None:
+        for item in todo_item.extra:
+            if isinstance(item, ContentLine) and item.name == "CATEGORIES":
+                item.value = ",".join(self.categories or "")
+                return
+        todo_item.extra.append(
+            ContentLine(name="CATEGORIES", value=",".join(self.categories or ""))
         )
-        if todo_item is None:
-            raise Exception("Cannot find todo")
 
-        self.completed = Arrow.now() if self.completed is None else None
-        # TODO: check ics new version
-        # There is a type issue with ics, will be resolved in next version.
-        todo_item.completed = self.completed  # type: ignore
-        calendar.todos.add(todo_item)
-
-        with open(self.path, "w") as f:
-            f.writelines(calendar.serialize_iter())
-
-    def save(self) -> None:
-        calendar = Calendar()
-        todo_item = IcsTodo()
-
-        calendar_dir = os.path.expanduser("~/.local/share/vdirsyncer/calendar")
-        new_dir = os.path.join(calendar_dir, self.list)
-        os.makedirs(new_dir, exist_ok=True)
-
-        new_path = os.path.join(new_dir, f"{uuid1()}.ics")
-
+    def _add_attributes(self, todo_item: IcsTodo) -> None:
+        # TODO: Add due date
         todo_item.name = self.summary
         todo_item.description = self.description
         todo_item.location = self.location or ""
         todo_item.priority = self.priority
-        todo_item.extra.append(
-            ContentLine(name="CATEGORIES", value=",".join(self.categories or ""))
-        )
-        calendar.todos.add(todo_item)
+        todo_item.due = self.due
+        # TODO: find a way to fix types...
+        # either by using "arrow", updating ics or writing our own lib
+        todo_item.completed = self.completed  # type: ignore
+        self._add_categories(todo_item)
 
-        self.path = new_path
-
-        with open(self.path, "w") as f:
-            f.writelines(calendar.serialize_iter())
-
-    def delete(self) -> None:
-        if self.path is None:
-            raise Exception(f"Cannot delete {self.summary} because paht is null")
-        os.remove(self.path)
+    def mark_as_done(self) -> None:
+        self.completed = datetime.now() if self.completed is None else None
