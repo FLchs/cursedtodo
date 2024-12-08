bb5e5f83bd6e1552f61aa0d8e77976cb18e80780
François Lachèse <francois.lachese@gmail.com>
2024-12-08 11:22:45 +0100

    fix: parsing rounder_borders from config

--- src/cursedtodo/utils/window_utils.py
+++ src/cursedtodo/utils/window_utils.py
@@ -6,10 +6,11 @@ from cursedtodo.utils.config import Config
 
 def add_borders(window: window) -> None:
     max_y, max_x = window.getmaxyx()
-    # Can't directly use chars in box() or border() beceause of utf8 
+    # Can't directly use chars in box() or border() beceause of utf8
     try:
         window.box()
-        if  bool(Config.get("UI", "rounded_borders")):
+        if Config.getboolean("UI", "rounded_borders"):
+
             window.addch(0, max_x - 1, "╮")
             window.addch(0, 0, "╭")
             window.addch(max_y - 1, 0, "╰")
@@ -17,7 +18,8 @@ def add_borders(window: window) -> None:
     except Exception:
         pass
 
-def draw_line(window: window, y:int, max_x: int) -> None:
+
+def draw_line(window: window, y: int, max_x: int) -> None:
     window.addch(y, 0, "├")
-    window.hline(curses.ACS_HLINE, max_x-2)
-    window.addch(y, max_x-1,"┤")
+    window.hline(curses.ACS_HLINE, max_x - 2)
+    window.addch(y, max_x - 1, "┤")
