fabb3846ca629152cd6dcf4e4d37b40c27954af2
François Lachèse <francois.lachese@gmail.com>
2024-12-08 11:20:40 +0100

    feat: add cli arguments feature, allow config file path argument

--- src/cursedtodo/utils/config.py
+++ src/cursedtodo/utils/config.py
@@ -1,7 +1,18 @@
 from configparser import ConfigParser
 import os
+from argparse import ArgumentParser
 
-xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or os.path.join(os.path.expanduser('~'), '.config')
+# TODO: this does not spark joy
+
+xdg_config_home = os.environ.get("XDG_CONFIG_HOME") or os.path.join(
+    os.path.expanduser("~"), ".config"
+)
+default_config_path = os.path.join(xdg_config_home, "cursedtodo/config.ini")
+
+parser = ArgumentParser(description="Your program description")
+parser.add_argument("-c", "--config", help="Config file path")
+config_file = parser.parse_args().config or default_config_path
+
 
 Config = ConfigParser()
-Config.read(os.path.join(xdg_config_home,"cursedtodo/config.ini"))
+Config.read(config_file)
